651ed3663469e20126bb997787f50893bb89d6f9
Bernard Sapida <BERNARDSAPIDA1706@GMAIL.COM>
2023-07-16 17:38:04 +0800

    My first react native

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your task'/>
+        <Button title='Add Task' />
+      </View>
+      <View style={styles.goalContainer}>
+        <Text style={styles.goalTitle}>List of Task</Text>
+        <Text style={styles.goalList}>Magcode</Text>
+        <Text style={styles.goalList}>Magaral</Text>
+        <Text style={styles.goalList}>Hangout with friends</Text>
+        <Text style={styles.goalList}>Kayquit Portal System</Text>
+        <Text style={styles.goalList}>Play Sky</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    margin: 50,
   },
+  inputContainer: {
+    flexDirection: 'row',
+    alignContent: 'center',
+    justifyContent: 'space-between',
+    gap: 5,
+    width: '100%',
+    padding: 5
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '70%',
+    borderRadius: 5,
+    padding: 5
+  },
+  goalContainer: {
+    marginTop: 10
+  },
+  goalTitle: {
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: '500'
+  },
+  goalList: {
+    fontSize: 18,
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 5,
+    elevation: 0.5,
+    backgroundColor: '#fafafa',
+    shadowOffset: { width: 10, height: 10 },
+    shadowOpacity: -1,
+    shadowRadius: 10,
+    overflow: 'hidden',
+  }
 });
